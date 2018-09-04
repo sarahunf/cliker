bf80dedcfdc884ed302cc60e0e91862cb8ac8f3d
sarahunf <42947247+sarahunf@users.noreply.github.com>
2018-09-03 22:33:06 -0300

    tutorial j#9
    add disaster, sounds, bg
    global bakers and shop not working properly.

--- Assets/Scripts/Shops/GlobalShop.cs
+++ Assets/Scripts/Shops/GlobalShop.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GlobalBaker : MonoBehaviour
-{
+public class GlobalShop : MonoBehaviour {
+
 	public GameObject realBtn;
 	public GameObject realBtnText;
 	public GameObject fakeBtn;
 	public GameObject fakeBtnText;
 	public int currentCash;
 
-	public static int bakerValue = 20;
+	public static int cashMakerValue = 5;
 	public static bool turnOffBtn = false;
+	public GameObject casherStats;
+	public static int numberOfCashers;
+	public static int cashersPerSec;
 
 	void Update ()
 	{
 		currentCash = GlobalCash.cashCount;
-		fakeBtnText.GetComponent<Text> ().text = "Buy auto - $" + bakerValue;
-		realBtnText.GetComponent<Text> ().text = "Buy auto - $" + bakerValue;
+		casherStats.GetComponent<Text> ().text = "cashers: " + numberOfCashers + "/" + cashersPerSec + " per sec";
+		fakeBtnText.GetComponent<Text> ().text = "Buy auto cash - $" + cashMakerValue;
+		realBtnText.GetComponent<Text> ().text = "Buy auto cash - $" + cashMakerValue;
 
-		if (currentCash >= bakerValue) {
+		if (currentCash >= cashMakerValue) {
 			realBtn.SetActive (true);
 			fakeBtn.SetActive (false);
 		}
